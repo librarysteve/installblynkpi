f686f17df104f5c09f1a4418aeaa2500cdf15ea8
skyline5ten0grapher <42779367+10rd5hiv4@users.noreply.github.com>
2019-05-18 18:29:55 -0400

    changed apt list from array to simple string

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 user=$(whoami)
-apt_needed=('git' 'python-pip' 'python3-pip' 'oracle-java8-jdk')
+apt_needed="git python-pip python3-pip oracle-java8-jdk"
 
 blynk_server="https://github.com/blynkkk/blynk-server/releases/download/v0.41.5/server-0.41.5-java8.jar"
 raspAp_repo="https://git.io/voEUQ"
@@ -9,7 +9,7 @@ raspAp_repo="https://git.io/voEUQ"
 
 installFunk() {
 	echo "Installing apt packages"
-	apt install ${apt_needed[@]}
+	apt install $apt_needed
 	wget "$blynk_server"
 	wget -q $raspAp_repo -O /tmp/raspap
 	bash /tmp/raspap < autoprompt.txt
