dce878f2b8781452473df26670b3b294551b9e09
LibrarySteve <sgarrison@lexpublib.org>
2019-09-03 15:57:59 -0400

    Update start_blynk_server.sh

--- start_blynk_server.sh
+++ start_blynk_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$(java -jar server-0.41.10-java8.jar -dataFolder /home/pi/blynk_service/data_folder)
+$(sudo java -jar server-0.41.10-java8.jar -dataFolder /home/pi/blynk_service/data_folder)
